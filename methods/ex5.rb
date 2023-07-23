b88964458b83c3f74d9d33f3e1ee4742ee4e9909
alex j <130667503+jeffries17@users.noreply.github.com>
2023-07-23 13:30:29 +0200

    methods exercises

---
+++ methods/ex5.rb
@@ -0,0 +1,5 @@
+def car(make, model)
+  puts "#{make} #{model}"
+end
+
+puts car("Toyota", "Corolla")
---
+++ methods/ex8.rb
@@ -0,0 +1,6 @@
+def pick_name(name = 'Alex')
+  name
+end
+
+puts pick_name('Kevin') == 'Kevin'
+puts pick_name == 'Alex'
